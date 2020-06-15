6f52e33a1f7332354294cd39a138fbb2f5c2cc8c
dzionek <vburtoch@gmail.com>
2020-06-15 16:19:10 +0200

    Extract route to set up the app

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,4 +1,4 @@
-from flask import request
+from flask import request, render_template
 from typing import Tuple
 
 from .base import main
@@ -8,36 +8,27 @@ from .utils import *
 Routes for the main functionality of the app.
 """
 
-@main.route('/add-channel', methods=['POST'])
-def add_channel_ajax() -> Any:
-    """Take POST form with the parameter 'channelName', check if it is a unique valid channel name.
-    If it is valid, add it to database. Return JSON saying if adding a channel was successful.
+@main.route('/app')
+def setup_app() -> str:
+    """Get username, channels and messages from database and render the main app
+    template with them.
 
     Returns:
-        JSON response consisting of boolean 'success' attribute and optionally 'errorMessage'.
-
-    Raises:
-        ValueError: If channel_name is None.
+        Rendered template of the main app.
 
     """
+    channels = Channel.query.all()
+    messages = Message.query.all()
+
+    return render_template(
+        'app.html', username=current_user, channels=channels, messages=messages
+    )
+
+@main.route('/add-channel', methods=['POST'])
+def add_channel_ajax() -> Any:
+    """Take POST form with the parameter 'channelName' and add the channel to database."""
     channel_name = request.form.get('channelName')
-
-    if channel_name is None:
-        raise TypeError('Channel name must not be None.')
-    else:
-        channel_name = str(channel_name)
-
-    if channel_has_invalid_name(channel_name):
-        return jsonify({
-            'success': False, 'errorMessage': f'You cannot create a channel of the given name: {channel_name}'
-        })
-
-    elif channel_already_exists(channel_name):
-        return jsonify({'success': False, 'errorMessage': 'The channel already exists'})
-
-    else:
-        add_channel(channel_name)
-        return jsonify({'success': True})
+    add_channel(channel_name)
 
 @main.route('/get-messages', methods=['POST'])
 def get_messages_ajax() -> Any:
